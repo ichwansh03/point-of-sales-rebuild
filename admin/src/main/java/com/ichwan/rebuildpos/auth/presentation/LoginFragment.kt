2e45ee190d5cbf11c264474e79c650a8b48d5074
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-24 16:25:29 +0700

    feat login (demo): add login and register views

--- admin/src/main/java/com/ichwan/rebuildpos/auth/presentation/LoginFragment.kt
+++ admin/src/main/java/com/ichwan/rebuildpos/auth/presentation/LoginFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.ichwan.rebuildpos.R
+import com.ichwan.rebuildpos.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.apply {
+
+            buttonRegister.setOnClickListener {
+                Navigation.findNavController(requireView()).navigate(R.id.action_to_registerFragment)
+            }
+        }
+
+        return binding.root
     }
 
 }
