ef1e47f465399c8a2e4d16d2c27691016b2857dd
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-30 23:06:23 +0700

    chore: navigate features

--- admin/src/main/java/com/ichwan/rebuildpos/MainActivity.kt
+++ admin/src/main/java/com/ichwan/rebuildpos/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ichwan.rebuildpos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.ichwan.rebuildpos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavBar, navController)
     }
 }
