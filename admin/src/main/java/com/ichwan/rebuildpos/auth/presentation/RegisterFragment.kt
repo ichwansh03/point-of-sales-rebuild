8765d1d81712330375a6831af87d6b8ea6afed38
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-30 23:08:43 +0700

    chore (auth): add register service

--- admin/src/main/java/com/ichwan/rebuildpos/auth/presentation/RegisterFragment.kt
+++ admin/src/main/java/com/ichwan/rebuildpos/auth/presentation/RegisterFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.ichwan.rebuildpos.R
+import com.ichwan.rebuildpos.auth.data.RegisterBusinessUser
+import com.ichwan.rebuildpos.auth.domain.RegisterService
 import com.ichwan.rebuildpos.databinding.FragmentRegisterBinding
 
-class RegisterFragment : Fragment() {
+class RegisterFragment : Fragment(), RegisterService {
 
     private lateinit var binding: FragmentRegisterBinding
 
@@ -26,6 +28,39 @@ class RegisterFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun register(registerBusinessUser: RegisterBusinessUser) {
+        if (validateForm()){
+
+        }
+    }
+
+    private fun isFieldEmpty(field: String): Boolean {
+        return field.isEmpty()
+    }
+
+
+    override fun validateForm(): Boolean {
+        binding.apply {
+            if (isFieldEmpty(inputBusNameRegister.text.toString())
+                || isFieldEmpty(inputBusTypeRegister.text.toString())
+                || isFieldEmpty(inputOutletTypeRegister.text.toString())
+                || isFieldEmpty(inputBusAddressRegister.text.toString())
+                || isFieldEmpty(inputNameRegister.text.toString())
+                || isFieldEmpty(inputAddressRegister.text.toString())
+                || isFieldEmpty(inputWhatsappRegister.text.toString())
+                || isFieldEmpty(inputPosRegister.text.toString())
+                || isFieldEmpty(inputEmailRegister.text.toString())
+                || isFieldEmpty(inputPasswordRegister.text.toString())
+                || isFieldEmpty(inputConfPasswordRegister.text.toString())) {
+                return false
+            } else if (inputPasswordRegister != inputConfPasswordRegister) {
+                return false
+            }
+        }
+
+        return true
     }
 
 }
